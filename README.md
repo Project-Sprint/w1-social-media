bb3799bde2762d1e5749709711312229a6048eb7
nandonyata <nandonp02@gmail.com>
2024-04-28 21:39:41 +0700

    feat: init .env, makefile

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # w1-social-media
 CatsSocial adalah aplikasi dimana pemilik kucing dapat saling menjodoh-jodohkan kucingnya
 
+export POSTGRESQL_URL='postgres://postgres:password@localhost:5432/your_database?sslmode=disable'
 
 go install -tags 'postgres' github.com/golang-migrate/migrate/v4/cmd/migrate@latest
 
--- main.go
+++ main.go
@@ -1,16 +1,33 @@
 package main
 
 import (
+	"fmt"
+	"log"
+
 	"github.com/gofiber/fiber/v2"
+	"github.com/joho/godotenv"
 )
 
-func main() {
+var PORT string
 
+func init() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatalf("Error loading .env file %v", err)
+	}
+
+	if PORT == "" {
+		PORT = "8080"
+	}
+
+}
+
+func main() {
 	app := fiber.New()
 
 	app.Get("/", func(c *fiber.Ctx) error {
 		return c.SendString("Hello, World!")
 	})
 
-	app.Listen(":3000")
+	app.Listen(fmt.Sprintf(":%s", PORT))
 }
